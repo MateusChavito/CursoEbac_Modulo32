93bcd3ab526e06fe15750abff40c562eb9c1b454
MateusChavito <mateus.chavito@hotmail.com>
2024-09-23 16:51:58 -0300

    Adicionando classe produto do modulo

---
+++ src/main/java/Domain/Produto.java
@@ -0,0 +1,51 @@
+package Domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "preco", nullable = false)
+    private double preco;
+
+    public Produto() {}
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
+
